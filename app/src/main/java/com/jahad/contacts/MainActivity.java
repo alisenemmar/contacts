5ece98d5675f37008be9facdcc572d65d81937b0
alisenemmar <app.developer.als@gmail.com>
2025-08-20 19:43:56 +0330

    4 -  1. Define variables + 2. Define function + 3. Define views in java + 4. onClick Event

--- app/src/main/java/com/jahad/contacts/MainActivity.java
+++ app/src/main/java/com/jahad/contacts/MainActivity.java
@@ -1,6 +1,10 @@
 package com.jahad.contacts;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // 3 + 4.5 = 7.5
+        // x + y = result
+        int x = 3;
+        float y = 4.5f;
+        float result = sum(x,y);
+
+        TextView color = findViewById(R.id.color);
+        Button bt1 = findViewById(R.id.bt1); // سبز
+        Button bt2 = findViewById(R.id.bt2); // قرمز
+        Button bt3 = findViewById(R.id.bt3); // آبی
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color.setText("سبز");
+                color.setTextColor(Color.GREEN);
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color.setText("قرمز");
+                color.setTextColor(Color.RED);
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color.setText("آبی");
+                color.setTextColor(Color.BLUE);
+            }
+        });
+
     }
+
+    float sum(int x, float y) {
+        return x+y;
+    }
+
 }
